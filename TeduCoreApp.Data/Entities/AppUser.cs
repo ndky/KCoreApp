376c6eb17ebaf1a2a26c77f3a00a99e3a4c26664
kynd <ky.nguyendinh@gmail.com>
2018-06-06 17:34:38 +0700

    Lesson12: Data Access Layer

--- TeduCoreApp.Data/Entities/AppUser.cs
+++ TeduCoreApp.Data/Entities/AppUser.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 using TeduCoreApp.Data.Enums;
 using TeduCoreApp.Data.Interfaces;
 
@@ -12,15 +10,11 @@ namespace TeduCoreApp.Data.Entities
     public class AppUser : IdentityUser<Guid>, IDateTracking, ISwitchable
     {
         public string FullName { get; set; }
-
-        public DateTime? BirthDay { set; get; }
-
+        public DateTime? Birthday { get; set; }
         public decimal Balance { get; set; }
-
         public string Avatar { get; set; }
-
+        public Status Status { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
-        public Status Status { get; set; }
     }
 }
